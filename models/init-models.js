fb5eead5d637eec3cd1c437ab188a39da5965bfa
Zouhir Bensmaia <freelancebenz@gmail.com>
2024-05-30 19:39:57 -0400

    D6

--- models/init-models.js
+++ models/init-models.js
@@ -1,9 +1,12 @@
 var DataTypes = require("sequelize").DataTypes;
+var _Testing = require("./Testing");
 
 function initModels(sequelize) {
+  var Testing = _Testing(sequelize, DataTypes);
 
 
   return {
+    Testing,
   };
 }
 module.exports = initModels;
