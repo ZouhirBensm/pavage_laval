658c3bd7165058e340c6a55367eaa568e384664b
Zouhir Bensmaia <freelancebenz@gmail.com>
2024-05-31 12:45:02 -0400

    localbusiness schema not appearing

--- public/js/schema/LocalBusinessSchema.js
+++ public/js/schema/LocalBusinessSchema.js
@@ -111,7 +111,7 @@ const localbusinessSchema = {
 
 const script2 = document.createElement('script');
 script2.type = 'application/ld+json';
-const text2 = JSON.stringify(websiteSchema)
+const text2 = JSON.stringify(localbusinessSchema)
 console.log("localbusinessSchema:" ,text2)
 script2.text = text2
 document.head.appendChild(script2);
