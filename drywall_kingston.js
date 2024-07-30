d8a94b948a5fd442388a0202f757dc4fd1b40894
Zouhir Bensmaia <freelancebenz@gmail.com>
2024-07-30 11:16:37 -0400

    added backlink fashipro

--- drywall_kingston.js
+++ drywall_kingston.js
@@ -483,6 +483,11 @@ app.get('/sitemap/xml-sitemap', (req, res) => {
   let last_modified_4 = '2024-06-24T13:13:22.821Z';
   let last_modified_4_date = new Date(last_modified_4);
 
+  // let last_modified_5 = '2024-07-26T15:20:26.052Z';
+  // let last_modified_5_date = new Date(last_modified_5);
+
+  
+
   // let last_modified_5 = '2024-06-25T17:29:12.963Z'
   // let last_modified_5_date = new Date(last_modified_5);
 
