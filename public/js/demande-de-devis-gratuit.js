3ec802ff18ae4e72120208903fac8fa512c98acc
Zouhir Bensmaia <freelancebenz@gmail.com>
2024-08-10 11:46:58 -0400

    updated the new email

--- public/js/demande-de-devis-gratuit.js
+++ public/js/demande-de-devis-gratuit.js
@@ -1,34 +1,7 @@
-// function sendEmail(event) {
-//   event.preventDefault(); // Prevent the form from submitting normally
-  
-//   // Get form data
-//   var name = document.getElementById('contact_name').value;
-//   // var email = document.getElementById('contact_email').value;
-//   // var phone = document.getElementById('contact_phone').value;
-//   // var message = document.getElementById('contact_message').value;
-
-
-//   console.log(name)
-
-//   // Construct the mailto link
-//   var mailtoLink = 'mailto:' + email +
-//     '?subject=' + encodeURIComponent('Free Quote Request') +
-//     '&body=' + encodeURIComponent(
-//       'Name: ' + name + '\n' 
-//       // +
-//       // 'Phone: ' + phone + '\n' +
-//       // 'Message: ' + message
-//     );
-
-//   // Open the mailto link in the default email client
-//   window.location.href = mailtoLink;
-// }
-
-
 function sendEmail(event) {
   event.preventDefault(); // Prevent the form from submitting normally
 
-  const site_rentee_email = 'drywallkingston@outlook.com'
+  const site_rentee_email = 'pavagegatineau@outlook.com'
 
   // Get form data
   var name = document.getElementById('contact_name').value;
