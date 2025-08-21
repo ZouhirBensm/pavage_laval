ad2abd26e0527371c6667973e8466d4a8689fecc
Zouhir Bensmaia <freelancebenz@gmail.com>
2025-08-21 12:20:13 -0400

    Added the dates for home pages

--- public/js/schema/WebSiteSchema.js
+++ public/js/schema/WebSiteSchema.js
@@ -4,7 +4,9 @@ const websiteSchema = {
   "@context": "https://schema.org",
   "@type": "WebSite",
   "url": `${business_data.website_main_url}`,
-  "name": `${business_data.business_name}`
+  "name": `${business_data.business_name}`,
+  "datePublished": business_data.home_page_published_date,
+  "dateModified": business_data.home_page_updated_date
 };
 
 const script1 = document.createElement('script');
