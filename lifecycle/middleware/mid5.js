c263b860fd7814da22f4292addc23ac01e4d8297
Zouhir Bensmaia <freelancebenz@gmail.com>
2025-08-24 11:38:39 -0400

    Done adding new french main services, and also added the extra french services with their respective english pages.

--- lifecycle/middleware/mid5.js
+++ lifecycle/middleware/mid5.js
@@ -36,7 +36,7 @@ async function mid1(req, res, next) {
   }
 
 
-  // console.log('\n\n(1)\n\n', db_extra_service_page_fr)
+  console.log('\n\n(1)\n\n', db_extra_service_page_fr)
 
 
   if (!db_extra_service_page_fr) {
