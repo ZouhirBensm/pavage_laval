1cb5951a9896da41ab65c0d79fe8fe465d137015
Zouhir Bensmaia <freelancebenz@gmail.com>
2025-09-07 09:15:14 -0400

    done with emails

--- public/js/contact-form-js.js
+++ public/js/contact-form-js.js
@@ -2,47 +2,46 @@ function sendEmail(event) {
 
   event.preventDefault();
 
-  const email_1 = 'pavage-asphalte-laval-montreal@outlook.com'
-  const email_2 = 'info@asphaltesolution.com';
-  
+  const email_1 = 'pavage-asphalte-laval-montreal@outlook.com'; // mailto
+  let optional_email_2 = undefined;
+  // optional_email_2 = 'earnanswers@outlook.com';
   let optional_email_3 = undefined;
   // optional_email_3 = 'pavage-asphalte-laval-montreal@outlook.com';
   let optional_email_4 = undefined;
   // optional_email_4 = 'earnanswers@outlook.com';
 
   // Get form data
-  var name = document.getElementById('contact_name').value;
-  var email = document.getElementById('contact_email').value;
-  var phone = document.getElementById('contact_phone').value;
-  var message = document.getElementById('contact_message').value;
+  const name = document.getElementById('contact_name').value;
+  const email = document.getElementById('contact_email').value;
+  const phone = document.getElementById('contact_phone').value;
+  const message = document.getElementById('contact_message').value;
 
-
-  console.log(name, email)
-  // console.log(phone, message)
+  console.log(name, email);
 
   // Build the recipient list
-  let recipientList = email_1;  
-  let ccList = email_2;
+  const recipientList = email_1;
+  const ccEmails = [];
 
-  if (optional_email_3) {
-    ccList += ',' + optional_email_3;
-  }
+  // Add optional emails to ccEmails array if they are defined
+  if (optional_email_2) ccEmails.push(optional_email_2);
+  if (optional_email_3) ccEmails.push(optional_email_3);
+  if (optional_email_4) ccEmails.push(optional_email_4);
 
-  if (optional_email_4) {
-    ccList += ',' + optional_email_4;
-  }
 
   // Construct the mailto link
-  var mailtoLink = 'mailto:' + recipientList +
-  '?cc=' + encodeURIComponent(ccList) +
-  '&subject=' + encodeURIComponent('Pavage Asphalte Laval Montreal: Quote Request') +
-  '&body=' + encodeURIComponent(
-    'Name: ' + name + '\n' +
-    'Phone: ' + phone + '\n' +
-    'Email: ' + email + '\n' +
-    'Message: ' + message
-  );
+  let mailtoLink = 'mailto:' + recipientList +
+    '?subject=' + encodeURIComponent('Pavage Gatineau: Quote Request') +
+    '&body=' + encodeURIComponent(
+      'Name: ' + name + '\n' +
+      'Phone: ' + phone + '\n' +
+      'Email: ' + email + '\n' +
+      'Message: ' + message
+    );
 
+  // Only add CC if there are any CC emails
+  if (ccEmails.length > 0) {
+    mailtoLink = mailtoLink.replace('?', '?cc=' + encodeURIComponent(ccEmails.join(',')) + '&');
+  }
 
   // Open the mailto link in the default email client
   window.location.href = mailtoLink;
