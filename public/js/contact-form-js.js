df8b413840526d596256ed675249785f977985e5
Zouhir Bensmaia <freelancebenz@gmail.com>
2025-09-06 20:54:58 -0400

    fixed emails

--- public/js/contact-form-js.js
+++ public/js/contact-form-js.js
@@ -2,10 +2,13 @@ function sendEmail(event) {
 
   event.preventDefault();
 
-  const site_rentee_email = 'info@asphaltesolution.com'
-  let site_rentee_second_optional_email = undefined;
-  site_rentee_second_optional_email = 'pavage-asphalte-laval-montreal@outlook.com';
-  const second_email = 'earnanswers@outlook.com';
+  const email_1 = 'pavage-asphalte-laval-montreal@outlook.com'
+  const email_2 = 'info@asphaltesolution.com';
+  
+  let optional_email_3 = undefined;
+  // optional_email_3 = 'pavage-asphalte-laval-montreal@outlook.com';
+  let optional_email_4 = undefined;
+  // optional_email_4 = 'earnanswers@outlook.com';
 
   // Get form data
   var name = document.getElementById('contact_name').value;
@@ -18,20 +21,28 @@ function sendEmail(event) {
   // console.log(phone, message)
 
   // Build the recipient list
-  let recipientList = site_rentee_email + ',' + second_email;
-  if (site_rentee_second_optional_email) {
-    recipientList += ',' + site_rentee_second_optional_email;
+  let recipientList = email_1;  
+  let ccList = email_2;
+
+  if (optional_email_3) {
+    ccList += ',' + optional_email_3;
+  }
+
+  if (optional_email_4) {
+    ccList += ',' + optional_email_4;
   }
 
   // Construct the mailto link
   var mailtoLink = 'mailto:' + recipientList +
-    '?subject=' + encodeURIComponent('Pavage Asphalte Laval Montreal: Quote Request') +
-    '&body=' + encodeURIComponent(
-      'Name/Nom: ' + name + '\n' +
-      'Phone/Numéro: ' + phone + '\n' +
-      'Email: '  + email + '\n' +
-      'Message: ' + message
-    );
+  '?cc=' + encodeURIComponent(ccList) +
+  '&subject=' + encodeURIComponent('Pavage Asphalte Laval Montreal: Quote Request') +
+  '&body=' + encodeURIComponent(
+    'Name: ' + name + '\n' +
+    'Phone: ' + phone + '\n' +
+    'Email: ' + email + '\n' +
+    'Message: ' + message
+  );
+
 
   // Open the mailto link in the default email client
   window.location.href = mailtoLink;
