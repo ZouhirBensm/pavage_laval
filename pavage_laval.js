cc7d14628dcd903b5dc1b653d0e846c0e510e6a9
Zouhir Bensmaia <freelancebenz@gmail.com>
2026-02-07 16:39:01 -0500

    Done adding the backlink pages into html sitemap.

--- pavage_laval.js
+++ pavage_laval.js
@@ -635,356 +635,47 @@ app.get('/backlink/:n',
 app.get('/sitemap/sitemap-3', sitemap_controller.cont1);
 
 
-// app.get('/sitemap/sitemap-3', async (req, res, next) => {
 
 
-//   res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
-//   res.setHeader('Pragma', 'no-cache');
-//   res.setHeader('Expires', '0');
 
 
 
-//   // Define the path to the XML file
-//   const xmlFilePath = path.join(__dirname, 'public', 'sitemap', 'sitemap.xml');
 
-//   // Delete the existing XML file if it exists
-//   if (fs.existsSync(xmlFilePath)) {
-//     fs.unlinkSync(xmlFilePath);
-//     console.log('Deleted existing sitemap.xml file');
-//   }
 
-//   const now = new Date();
-//   console.log('now -> ', now);
 
-//   let last_modified_1 = '2024-08-14T00:34:21.928Z';
-//   let last_modified_1_date = new Date(last_modified_1);
 
 
-//   let urls = []
-  
-//   const discarded_object_ids = [8, 9, 10];
 
 
-//   // French pages
-//   const all_data_per_page_fr = await db.all_data_per_page_fr.findAll({
-//     raw: true
-//   });
 
-//   if (!all_data_per_page_fr) {
-//     const error = new Error("No service pages found!")
-//     return next(error)
-//   }
 
-//   all_data_per_page_fr.forEach(all_data_per_page_fr => {
 
-//     if (discarded_object_ids.includes(all_data_per_page_fr.id)) { return; }
 
-//     let lastmod = new Date(all_data_per_page_fr.last_modified)
+app.get(['/plan-du-site', '/sitemap/en'],
+  middleware9.mid1,
+  middleware9_en.mid1,
+  async (req, res, next) => {
 
-//     urls.push({
-//       URL: all_data_per_page_fr.page_url_identify,
-//       lastmod: lastmod,
-//       changefreq: "monthly",
-//       priority: 1,
-//       alt: all_data_per_page_fr.eq_lang_page,
-//       alt_lang: 'en'
-//     });
-//   });
+    let backlink_pages_urls = []
+    const backlinksDir = path.join(__dirname, './backlinks');
 
+    const files = fs.readdirSync(backlinksDir);
 
+    for (const file of files) {
+      const match = file.match(/^backlink(\d+)\.txt$/i);
+      if (!match) continue;
 
+      const number = match[1];
 
+      backlink_pages_urls.push(`/backlink/${number}`);
+    }
 
-//   // English pages
-//   const all_data_per_page_en = await db.all_data_per_page_en.findAll({
-//     raw: true
-//   });
+    res.locals.backlink_pages_urls = backlink_pages_urls
+    // console.log("\n\n________________________ \n\nres.locals.index_page_data -> \n\n", res.locals.index_page_data)
+    // return res.end()
 
-//   if (!all_data_per_page_en) {
-//     const error = new Error("No service pages found!")
-//     return next(error)
-//   }
-
-//   all_data_per_page_en.forEach(all_data_per_page_en => {
-
-//     if (discarded_object_ids.includes(all_data_per_page_en.id)) { return; }
-
-//     let lastmod = new Date(all_data_per_page_en.last_modified)
-
-//     urls.push({
-//       URL: all_data_per_page_en.page_url_identify,
-//       lastmod: lastmod,
-//       changefreq: "monthly",
-//       priority: 1,
-//       alt: all_data_per_page_en.eq_lang_page,
-//       alt_lang: 'fr'
-//     });
-//   });
-
-
-
-//   // console.log(urls)
-//   // return res.end()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//   const extra_service_pages_fr = await db.extra_service_page_fr.findAll({
-//     attributes: ['slug', 'title', 'last_modified', 'eq_lang_page'],
-//     raw: true
-//   });
-
-//   if (!extra_service_pages_fr) {
-//     const error = new Error("No service pages found!")
-//     return next(error)
-//   }
-
-
-//   extra_service_pages_fr.forEach(extra_service_page_fr => {
-//     let url = `/service/${extra_service_page_fr.slug}`;
-//     let alt = `/service/${extra_service_page_fr.eq_lang_page}`
-
-//     // console.log(extra_service_page_fr.last_modified)
-//     let lastmod = new Date(extra_service_page_fr.last_modified)
-
-
-//     urls.push({
-//       URL: url,
-//       lastmod: lastmod,
-//       changefreq: "monthly",
-//       priority: 1,
-//       alt: alt,
-//       alt_lang: 'en'
-//     });
-//   });
-
-
-
-
-
-//   const extra_service_pages_en = await db.extra_service_page_en.findAll({
-//     attributes: ['slug', 'title', 'last_modified', 'eq_lang_page'],
-//     raw: true
-//   });
-
-//   if (!extra_service_pages_en) {
-//     const error = new Error("No service pages found!")
-//     return next(error)
-//   }
-
-
-//   extra_service_pages_en.forEach(extra_service_page_en => {
-//     let url = `/service/${extra_service_page_en.slug}`;
-//     let alt = `/service/${extra_service_page_en.eq_lang_page}`
-
-//     // console.log(extra_service_page_en.last_modified)
-//     let lastmod = new Date(extra_service_page_en.last_modified)
-
-
-//     urls.push({
-//       URL: url,
-//       lastmod: lastmod,
-//       changefreq: "monthly",
-//       priority: 1,
-//       alt: alt,
-//       alt_lang: 'fr'
-//     });
-//   });
-
-
-
-
-
-
-
-
-
-
-
-
-
-//   // console.log(urls)
-//   // return res.end()
-
-
-
-
-
-
-
-//   const blog_elements_fr = await db.blog_element_fr.findAll({
-//     attributes: ['slug', 'title', 'datetime_edited'],
-//     include: [
-//       {
-//         model: db.category_fr,
-//         as: 'category',
-//         attributes: ['category_name', 'slug']
-//       }
-//     ],
-//     raw: true,
-//     nest: true
-//   });
-
-//   if (!blog_elements_fr) {
-//     const error = new Error("No blog elements found!")
-//     return next(error)
-//   }
-
-
-//   const blog_elements_en = await db.blog_element_en.findAll({
-//     attributes: ['slug', 'title', 'datetime_edited'],
-//     include: [
-//       {
-//         model: db.category_en,
-//         as: 'category',
-//         attributes: ['category_name', 'slug']
-//       }
-//     ],
-//     raw: true,
-//     nest: true
-//   });
-
-//   if (!blog_elements_en) {
-//     const error = new Error("No blog elements found!")
-//     return next(error)
-//   }
-
-
-//   // console.log("blog_elements_en -> ", blog_elements_en)
-
-
-//   // Remove '/en' from category.slug for each element in blog_elements_en
-//   const blog_elements_en_updated = blog_elements_en.map(element => {
-//     // Check if category.slug ends with '/en' and remove it
-//     if (element.category.slug.endsWith('/en')) {
-//       element.category.slug = element.category.slug.replace(/\/en$/, '');
-//     }
-//     return element;
-//   });
-
-
-//   // console.log("blog_elements_en_updated -> ", blog_elements_en_updated)
-//   // return res.end()
-
-
-//   const blog_elements = [...blog_elements_fr, ...blog_elements_en_updated]
-
-
-//   // console.log("blog_elements -> ", blog_elements)
-//   // return res.end()
-
-
-
-
-//   blog_elements.forEach(blog_element => {
-
-//     let url = `/blog/${blog_element.category.slug}/blog-posting/${blog_element.slug}`;
-
-//     let datetime_edited = new Date(blog_element.datetime_edited)
-
-//     urls.push({
-//       URL: url,
-//       lastmod: datetime_edited,
-//       changefreq: "monthly",
-//       priority: 0.8
-//     });
-
-
-//   });
-
-
-
-
-
-//   let last_modified_2 = '2025-09-20T10:27:07.077Z';
-//   let last_modified_2_date = new Date(last_modified_2);
-
-
-//   urls.push({
-//     URL: '/backlink/1',
-//     lastmod: last_modified_2_date,
-//     changefreq: "monthly",
-//     priority: 1,
-//   });
-
-
-
-//   console.log(urls)
-//   // return res.end()
-
-
-
-
-
-
-
-//   const xml = createSiteMap(urls, res.locals.protocoled_domain);
-
-//   console.log(xml)
-
-//   fs.writeFileSync(xmlFilePath, xml, 'utf-8');
-//   console.log('New sitemap.xml file generated');
-
-//   // return res.render('sitemap');
-//   // return res.sendFile('sitemap.html', { root: 'public' });
-//   return res.redirect(301, '/');
-//   // return res.end()
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.get(['/plan-du-site', '/sitemap/en'], middleware9.mid1, middleware9_en.mid1, async (req, res, next) => {
-
-
-
-
-  // console.log("\n\n________________________ \n\nres.locals.index_page_data -> \n\n", res.locals.index_page_data)
-
-
-
-  // return res.end()
-
-  return res.render('plan-du-site', { ...res.locals.index_page_data });
-
-});
+    return res.render('plan-du-site', { ...res.locals.index_page_data });
+  });
 
 
 
